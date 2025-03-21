034500a0890bb95c7354bd8c35fdbd9dab207017
cho6019 <j01040864205@gmail.com>
2025-03-21 10:41:43 +0900

    d

--- src/movie/api/call.py
+++ src/movie/api/call.py
@@ -47,3 +47,36 @@ def list2df_check_num(df: pd.DataFrame, l=num_cols):
     for c in l:
         df[c] = df[c].apply(pd.to_numeric)
     return df
+
+def fill_na_with_column(origin_df, c_name):
+    df = origin_df.copy()
+    for i, row in df.iterrows():
+        if pd.isna(row[c_name]):
+            same_movie_df = df[df["movieCd"] == row["movieCd"]]
+            notna_idx = same_movie_df[c_name].dropna().first_valid_index()
+            if notna_idx is not None:
+                df.at[i, c_name] = df.at[notna_idx, c_name]
+    return df
+
+def gen_unique(df: pd.DataFrame, drop_columns: list) -> pd.DataFrame:
+    df_drop = df.drop(columns=['rnum', 'rank', 'rankInten', 'salesShare'])
+    unique_df = df_drop.drop_duplicates()
+    return unique_df
+
+def re_ranking(df: pd.DataFrame) -> pd.DataFrame:
+    df["rnum"] = df["audiCnt"].rank(method="dense", ascending=False).astype(int)
+    df["rank"] = df["audiCnt"].rank(method="min", ascending=False).astype(int)
+    return df
+
+def combine_unique_parquet(base_path):
+    combined_df = pd.DataFrame()
+    parquet_files = [f for f in os.listdir(base_path) if f.endswith(".parquet")]
+    
+    for file in parquet_files:
+        file_path = os.path.join(base_path, file)
+        df = pd.read_parquet(file_path)
+        combined_df = pd.concat([combined_df, df], ignore_index=True)
+    
+    # 중복 제거
+    combined_df = combined_df.drop_duplicates()
+    return combined_df
--- tests/test_call.py
+++ tests/test_call.py
@@ -1,4 +1,7 @@
-from movie.api.call import gen_url, call_api, list2df, save_df, list2df_check_num
+from movie.api.call import (
+    gen_url, call_api, list2df, save_df,
+    fill_na_with_column, gen_unique, re_ranking
+)
 import os
 import pandas as pd
 from pandas.api.types import is_numeric_dtype
@@ -47,26 +50,33 @@ def test_save_df():
     assert 'dt' not in read_df.columns
     assert 'dt' in pd.read_parquet(base_path).columns
     
-def test_save_df_url_params():
-    ymd = "20210101"
-    url_params={'multiMovieYn': 'Y'}
+# def test_save_df_url_params():
+    # ymd = "20210101"
+    # url_params={'multiMovieYn': 'Y'}
     
-    data = call_api(ymd, url_params)
-    df = list2df(data, ymd, url_params)
-    base_path = "~/temp/movie"
-    r = save_df(df, base_path, ['dt'] + list(url_params.keys()))
-    assert r == f"{base_path}/dt={ymd}"
-    read_df = pd.read_parquet(r)
-    assert 'dt' not in read_df.columns
-    assert 'dt' in pd.read_parquet(base_path).columns
+    # data = call_api(ymd, url_params)
+    # df = list2df(data, ymd, url_params)
+    # base_path = "~/temp/movie"
+    # r = save_df(df, base_path, ['dt'] + list(url_params.keys()))
+    # assert r == f"{base_path}/dt={ymd}"
+    # read_df = pd.read_parquet(r)
+    # assert 'dt' not in read_df.columns
+    # assert 'dt' in pd.read_parquet(base_path).columns
     
-def test_list2df_check_num():
-    num_cols=['rnum', 'rank', 'rankInten', 'salesAmt', 'audiCnt',
-             'audiAcc', 'scrnCnt', 'showCnt', 'salesShare', 'salesInten', 'salesChange',
-             'audiInten', 'audiChange']
-    ymd=20120101
-    df = list2df(call_api(dt=ymd), ymd)
-    df = list2df_check_num(df, num_cols)
-    for c in num_cols:
-        assert is_numeric_dtype(df[c]), f'{c}가 숫자가 아님'
-        # assert df[c].dtype in ['int64', 'float64']
+def test_merge_df():
+    PATH = "~/data/movies/dailyboxoffice/dt=20240101"
+    df = pd.read_parquet(PATH)
+    assert len(df) == 50
+    
+    df1 = fill_na_with_column(df, 'multiMovieYn')
+    assert df1["multiMovieYn"].isna().sum() == 5
+    
+    df2 = fill_na_with_column(df1, 'repNationCd')
+    assert df2["repNationCd"].isna().sum() == 5
+
+    drop_columns=['rnum', 'rank', 'rankInten', 'salesShare']
+    unique_df = gen_unique(df=df2, drop_columns=drop_columns) 
+    assert len(unique_df) == 25
+    
+    new_ranking_df = re_ranking(unique_df)
+    assert new_ranking_df.iloc[0]['movieNm'] == '노량: 죽음의 바다'
